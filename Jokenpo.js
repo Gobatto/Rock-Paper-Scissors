96569d4b3b69cec6f74ce6dfa459fe2b54489ee8
Raphael Gobatto Laranjeira <raphagobatto@hotmail.com>
2022-07-21 11:56:57 -0300

    Update Jokenpo.js
    Jokenpo finalised

--- Jokenpo.js
+++ Jokenpo.js
@@ -1,6 +1,6 @@
 const getUserChoice = userInput => {
     userInput = userInput.toLowerCase();
-    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'rain') {
       return userInput;
      } else {
       console.log('Please, choose: rock, paper or scissors');
@@ -22,6 +22,9 @@ const getUserChoice = userInput => {
   const determineWinner = (userChoice, computerChoice) => {
     if (userChoice === computerChoice) {
       return 'The game is a tie!';
+    }
+    if (userChoice === 'rain') {
+    return 'You won!';
     }
     if (userChoice === 'rock') {
       if (computerChoice === 'paper') {
@@ -45,3 +48,13 @@ const getUserChoice = userInput => {
       }
     }
   };
+  
+  const playGame = () => {
+    const userChoice = getUserChoice('rock');
+    const computerChoice = getComputerChoice();
+    console.log(`You choose: ${userChoice}`);
+    console.log(`Computer choose: ${computerChoice}`);
+    console.log(determineWinner(userChoice, computerChoice));
+  }
+  
+  playGame();
