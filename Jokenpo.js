b56b4e08d6d698a7e684f70e8077ad60293ed858
Raphael Gobatto Laranjeira <raphagobatto@hotmail.com>
2022-07-21 11:06:08 -0300

    Update Jokenpo.js

--- Jokenpo.js
+++ Jokenpo.js
@@ -7,5 +7,40 @@ const getUserChoice = userInput => {
      }
   }
   
-  console.log(getUserChoice('Paper'));
+  const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+    };
+  }
   
+  const determineWinner = (userChoice, computerChoice)
+    if (userChoice === computerChoice) {
+      return 'The game is a tie!';
+    if (userChoice === 'rock') {
+      if (computerChoice === 'paper') {
+        return 'You lost!'
+      else
+        return 'You won!'
+      }
+    }
+    if  (userChoice === 'paper') {
+      if (computerChoice === 'scissors') {
+        return 'You lost!'
+      else
+        return 'You won!'
+      }
+    }
+    if  (userChoice === 'scissors') {
+      if (computerChoice === 'rock') {
+        return 'You lost!'
+      else
+        return 'You won!'
+      }
+    }
+  }
